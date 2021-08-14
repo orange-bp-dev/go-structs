cbfe71be76492467f5b34b9f9e26d3e11211209f
Sato Ryoka <satoryoka0920@gmail.com>
2021-08-14 16:29:57 +0900

    update

--- main.go
+++ main.go
@@ -6,11 +6,44 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName string
+	// contact -> contactInfoの場合contactInfoのみでOK
+	contact contactInfo
+}
+
+type contactInfo struct {
+	email string
+	zipCode int
 }
 
 func main() {
 	// プロパティ名を書かなくても前から順に当てはめることは出来るが、バグの原因になるので非推奨
 	ryoka := person{firstName: "Ryoka", lastName: "Sato"}
+	
+	var chiharu person
+	chiharu.firstName = "Chiharu"
+	chiharu.lastName = "Yamamoto"
+	
+	masatoshi := person{
+		firstName: "Masatoshi",
+		lastName: "Hamada",
+		contact: contactInfo{
+			email: "hamada@downtown.com",
+			zipCode: 2222222,
+		},
+	}
 	fmt.Println(ryoka)
+	fmt.Println(chiharu)
+
+	masatoshi.updateName("Hamachon")
+	//これでは名前が更新されていない！ => ポインタの概念
+	masatoshi.print()
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
 
